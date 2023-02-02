01176f02a4474c42dc61e7da4f5b67514e36a557
le-Pras <70028743+le-Pras@users.noreply.github.com>
2023-02-02 16:05:07 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,35 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { name$, storeDataOnServer, storedataOnServerError } from './external';
+import { Observable } from 'rxjs';
+// name$.subscribe((value) => console.log(value));
 
-name$.subscribe((value) => console.log(value));
+// storeDataOnServer('Some value').subscribe((value) => console.log(value));
 
-storeDataOnServer('Some value').subscribe((value) => console.log(value));
+// storedataOnServerError('Some Error value').subscribe({
+//   next: (value) => console.log(value),
+//   error: (err) => console.log('Error when saving', err.message),
+// });
 
-storedataOnServerError('Some Error value').subscribe({
-  next: (value) => console.log(value),
-  error: (err) => console.log('Error when saving', err.message),
+const obs$ = new Observable<string>((sub) => {
+  console.log('In observable...');
+  sub.next('Alice');
+  setTimeout(() => sub.next('Ben'), 2000);
+  setTimeout(() => sub.next('charlie'), 4000);
 });
+
+// const observer$ = {
+//   next: (value) => console.log(value),
+// };
+// obs$.subscribe(observer$);
+
+const subscription = obs$.subscribe((value) => console.log(value));
+
+setTimeout(() => {
+  console.log('unsubscription happening');
+  subscription.unsubscribe();
+}, 3000);
+
 @Component({
   selector: 'my-app',
   standalone: true,
